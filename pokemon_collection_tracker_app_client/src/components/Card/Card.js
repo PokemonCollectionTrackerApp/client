472821e377fad17a0bc0a6f897979541b54a6487
Forstrei <ajdraper96@gmail.com>
2023-02-22 22:40:29 -0700

    began formatting basic cards

--- pokemon_collection_tracker_app_client/src/components/Card/Card.js
+++ pokemon_collection_tracker_app_client/src/components/Card/Card.js
@@ -2,12 +2,40 @@ import React from 'react';
 import './Card.css';
 
 function Card(props) {
-  return (
-    <div className="Card">
-        <img src={$`props.species`} />
-        <p>{props.nickname}<br />{props.species}</p>
-    </div>
-  );
+    var type = props.type || "fairy"
+    var move1 = <tr>{props.move1 || "placehoder"}</tr>
+    if(props.move2){
+        var move2 = <tr>{props.move2}</tr>
+    }
+    if(props.move3){
+        var move3 = <tr>{props.move3}</tr>
+    }
+    if(props.move4){
+        var move4 = <tr>{props.move4}</tr>
+    }
+    if(props.isClicked == false){
+        return (
+            <div className="card">
+                <img src={`props.species`} />
+                <p>{props.nickname || "Godzirra"}</p>
+                <p className={type}>{props.species || "Tyranitar"}</p>
+            </div>
+          );
+    } else {
+        return (
+            <div className="card">
+                <img src={`props.species`} />
+                <p>{props.nickname || "Godzirra"}</p>
+                <p className={type}>{props.species || "Tyranitar"}</p>
+                <td className="moves">
+                    {move1}
+                    {move2}
+                    {move3}
+                    {move4}
+                </td>
+            </div>
+        )
+    }
 }
 
 export default Card;
