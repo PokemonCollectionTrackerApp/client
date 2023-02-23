8209389eb9da4cbee14b1d0083373fbb131be572
Forstrei <ajdraper96@gmail.com>
2023-02-22 22:39:34 -0700

    Added Card component to App.js

--- pokemon_collection_tracker_app_client/src/App.js
+++ pokemon_collection_tracker_app_client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Card from './components/Card/Card';
 
 function App() {
   return (
@@ -20,6 +21,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
